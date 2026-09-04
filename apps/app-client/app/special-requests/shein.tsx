131aec4d66feeec5f81758ecbbe656dd3a68c4a5
bthwani2-boop <242864241+bthwani2-boop@users.noreply.github.com>
2026-09-04 19:29:32 +0300

    refactor(app-client): remove premature semantic furnishing

--- apps/app-client/app/special-requests/shein.tsx
+++
@@ -1,2 +0,0 @@
-import { ClientRouteScreen } from "../../src/navigation/ClientRouteScreen";
-export default function SheinRoute() { return <ClientRouteScreen route={{ kind: "special-request-shein" }} />; }
