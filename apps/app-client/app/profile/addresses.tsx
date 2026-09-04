f61033a14b800a868dc5384b94648543db5f3d44
bthwani2-boop <242864241+bthwani2-boop@users.noreply.github.com>
2026-09-04 19:29:02 +0300

    refactor(app-client): remove premature semantic furnishing

--- apps/app-client/app/profile/addresses.tsx
+++
@@ -1,7 +0,0 @@
-import { useLocalSearchParams } from "expo-router";
-import { ClientRouteScreen, singleRouteParam } from "../../src/navigation/ClientRouteScreen";
-export default function AddressesRoute() {
-  const { returnStoreId: rawReturnStoreId } = useLocalSearchParams<{ returnStoreId?: string | string[] }>();
-  const returnStoreId = singleRouteParam(rawReturnStoreId);
-  return <ClientRouteScreen route={{ kind: "profile-addresses", ...(returnStoreId ? { returnStoreId } : {}) }} />;
-}
