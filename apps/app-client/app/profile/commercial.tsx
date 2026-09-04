d8fab09790010f8933d5364eac0e0b6212c87eea
bthwani2-boop <242864241+bthwani2-boop@users.noreply.github.com>
2026-09-04 19:29:06 +0300

    refactor(app-client): remove premature semantic furnishing

--- apps/app-client/app/profile/commercial.tsx
+++
@@ -1,2 +0,0 @@
-import { ClientRouteScreen } from "../../src/navigation/ClientRouteScreen";
-export default function CommercialProfileRoute() { return <ClientRouteScreen route={{ kind: "profile-commercial" }} />; }
